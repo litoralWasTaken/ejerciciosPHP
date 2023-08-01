697b1bc73248cb5f1b957fa7b20ce120110929f0
Rodrigo Garcia <litoralwastaken@gmail.com>
2023-08-01 18:23:55 +0200

    Added HTML elements as PHP variables

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<h1> Alumno de <span>EIP</span></h1>
+<?php
+const school = 'EIP';
+$name = 'Juan';
+$surname = 'Rodríguez Fernández';
+$age = 55;
+$interests = 'Deportes, música, naturaleza';
+
+?>
+<h1> Alumno de <span><?= school ?></span></h1>
 <p>
-    <b>Nombre: </b> <span> Juan </span>
+    <b>Nombre: </b> <span> <?= $name ?> </span>
+</p>
+<p>
+    <b>Apellidos: </b> <span> <?= $surname ?> </span>
+</p>
+<p>
+    <b>Edad: </b> <span> <?= $age ?> </span>
+</p>
+<p>
+    <b>Intereses: </b> <span> <?= $interests ?> </span>
 </p>

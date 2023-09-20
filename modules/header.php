1634b6c421ae1102a5d37ec173f2ad5dbf37cf34
Rodrigo Garcia <litoralwastaken@gmail.com>
2023-09-20 20:24:54 +0200

    Pasado header y footer a modulos

---
+++ modules/header.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.php">LOGO</a>
+    <ul class="nav justify-content-end">
+        <a class="link" href="servicios.php">Servicios</a>
+        <a class="link" href="contacto.php">Contacto</a>
+        <li class="nav-item">
+            <a class="btn btn-outline-light" href="#">Login</a>
+        </li>
+    </ul>
+</nav>

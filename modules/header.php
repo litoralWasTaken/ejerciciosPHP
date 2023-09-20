6d69493692d3ebc9e9fe640ec944ca3b55500dd6
Rodrigo Garcia <litoralwastaken@gmail.com>
2023-09-20 21:23:19 +0200

    Opcional: Clase activa en el navegador

--- modules/header.php
+++ modules/header.php
@@ -1,8 +1,21 @@
 <nav class="navbar navbar-dark bg-dark">
     <a class="navbar-brand" href="index.php">LOGO</a>
     <ul class="nav justify-content-end">
-        <a class="link" href="servicios.php">Servicios</a>
-        <a class="link" href="contacto.php">Contacto</a>
+        <?php
+        if ($_SERVER['REQUEST_URI'] == '/servicios.php') {
+            echo '<a class="link" id="link-active" href="servicios.php">Servicios</a>';
+        } else {
+            echo '<a class="link" href="servicios.php">Servicios</a>';
+        }
+
+        if ($_SERVER['REQUEST_URI'] == '/contacto.php') {
+            echo '<a class="link" id="link-active" href="contacto.php">Contacto</a>';
+        } else {
+            echo '<a class="link" href="contacto.php">Contacto</a>';
+        }
+        ?>
+
+
         <li class="nav-item">
             <a class="btn btn-outline-light" href="#">Login</a>
         </li>
